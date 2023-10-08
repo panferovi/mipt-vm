99acb8af3268334075b7d597767d78b082d42fb4
Ivan Panferov <panferov.ivan@huawei-partners.com>
2023-10-09 01:08:30 +0300

    Generate bytecode for bench. Code refactoring
    Signed-off-by: Ivan Panferov <panferov.ivan@huawei-partners.com>

--- file/bytecode.h
+++ file/bytecode.h
@@ -3,9 +3,45 @@
 
 #include <vector>
 
+#include "file/proto.h"
+
 namespace mipt_vm {
 
-class Instruction {};
+class Instruction {
+public:
+    // clang-format off
+    enum Type : uint8_t {
+        LDA,
+        LDA_I,
+        LDA_STR,
+        STA,
+        STA_OBJ,
+        ADD,
+        SUB,
+        MUL,
+        MULI,
+        DIV,
+        CALL,
+        RETURN
+    };
+    // clang-format on
+
+    union Immediate {
+        int64_t as_int;
+        uint64_t as_uint;
+        double as_double;
+    };
+
+    union OperandType {
+        uint8_t reg_name;
+        Immediate imm;
+        Proto *proto {nullptr};
+    };
+
+    Type type;
+    uint8_t reg_src_1 {0};
+    OperandType src_2;
+};
 
 class Bytecode {
 public:
---
+++ stdlib/io.h
@@ -0,0 +1,21 @@
+#ifndef STDLIB_IO_H_
+#define STDLIB_IO_H_
+
+#include <cstdint>
+
+namespace mipt_vm::stdlib {
+
+class IO {
+public:
+    static void printString(const char *str);
+
+    static void printlnString(const char *str);
+
+    static void printI64(int64_t val);
+
+    static void printlnI64(int64_t val);
+};
+
+}  // namespace mipt_vm::stdlib
+
+#endif  // STDLIB_IO_H_
